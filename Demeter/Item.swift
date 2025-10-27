6651641386646fe74c37bac38f18d4ddbd775fbb
gautier7 <gautier-colson@hotmail.com>
2025-10-28 00:53:56 +0800

    Initial Commit

---
+++ Demeter/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Demeter
+//
+//  Created by Gautier Colson on 28/10/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
